86957c91ccab964b2bd471805310ccc56da326c6
dolidev2 <dolidev2.0@gmail.com>
2023-09-19 15:06:52 +0000

    chat frontend is finished

--- resources/views/layouts/Carrier.blade.php
+++ resources/views/layouts/Carrier.blade.php
@@ -35,7 +35,7 @@
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.js"></script>
 <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-
+    @yield('styles')
 </head>
 <body>
     @include('layouts.carrier.carrier_header')
